6f75cadd7b4a401af842daf3b044dff50b3e53c7
waseemnazir <waseem.beigh@gmail.com>
2024-09-23 08:26:51 +0530

    feat: total pages

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,21 +1,31 @@
 // Molecules/Pagination.js
 import React from 'react';
 import Button from './Button';
+import './Pagination.css';
 
 const Pagination = ({ totalPages, currentPage, paginate }) => {
   return (
-    <div>
-      {Array.from({ length: totalPages }, (_, index) => (
-        <Button
-          key={index + 1}
-          onClick={() => paginate(index + 1)}
-          active={currentPage === index + 1}
-        >
-          {index + 1}
-        </Button>
-      ))}
+    <div className="pagination-container">
+      {/* Total Pages Info */}
+      <span className="pagination-info">
+        Page {currentPage} of {totalPages}
+      </span>
+
+      {/* Page Buttons */}
+      <div className="pagination-buttons">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            active={currentPage === index + 1}
+          >
+            {index + 1}
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Pagination;
+
